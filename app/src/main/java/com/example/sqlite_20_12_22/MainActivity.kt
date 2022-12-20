2aeddf1be8a1c059bb5c964ffc8631bb2384e0e7
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-12-20 08:40:30 +0100

    Añadido modelo y binding

--- app/src/main/java/com/example/sqlite_20_12_22/MainActivity.kt
+++ app/src/main/java/com/example/sqlite_20_12_22/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.sqlite_20_12_22
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.sqlite_20_12_22.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/sqlite_20_12_22/models/User.kt
@@ -0,0 +1,8 @@
+package com.example.sqlite_20_12_22.models
+
+data class User(
+    var id: Int,
+    var email: String,
+    var password: String,
+    var perfil: Int
+)
