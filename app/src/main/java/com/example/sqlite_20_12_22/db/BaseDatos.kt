0bcf8afb2a7310e2d2cc5fbd13815e511ba375e8
Pablo Sánchez López <65461723+PSL2001@users.noreply.github.com>
2022-12-20 08:52:11 +0100

    Añadido BaseDatos.kt y varios de sus metodos

---
+++ app/src/main/java/com/example/sqlite_20_12_22/db/BaseDatos.kt
@@ -0,0 +1,69 @@
+package com.example.sqlite_20_12_22.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.sqlite_20_12_22.models.User
+
+class BaseDatos(c: Context): SQLiteOpenHelper(c, DATABASE, null, VERSION) {
+    companion object {
+        val DATABASE = "Users"
+        val TABLE = "Users"
+        val VERSION = 1
+    }
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        p0?.execSQL("CREATE TABLE $TABLE (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL UNIQUE, password TEXT NOT NULL, perfil INTEGER NOT NULL)")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        p0?.execSQL("DROP TABLE IF EXISTS $TABLE")
+        onCreate(p0)
+    }
+
+    //Metodos para insertar, actualizar, eliminar y consultar (CRUD)
+    //Insertar
+    fun insertar(user: User): Long {
+        //Abrir la base de datos en modo escritura
+        val db = this.writableDatabase
+        //Crear un objeto de tipo ContentValues para almacenar los datos a insertar
+        val valores = ContentValues().apply {
+            put("email", user.email)
+            put("password", user.password)
+            put("perfil", user.perfil)
+        }
+        //Insertar los datos en la tabla
+        val res = db.insert(TABLE, null, valores)
+        //Cerrar la base de datos
+        db.close()
+        //Retornar el resultado
+        //Si res es -1, hubo un error
+        return res
+    }
+
+    //Leer
+    fun leer(): ArrayList<User> {
+        //Abrir la base de datos en modo lectura
+        val db = this.readableDatabase
+        //Crear un ArrayList para almacenar los datos leidos
+        val lista = ArrayList<User>()
+        //Crear un objeto de tipo Cursor para almacenar los datos leidos
+        val cursor = db.rawQuery("SELECT * FROM $TABLE", null)
+        //Recorrer el cursor y almacenar los datos en el ArrayList
+        if (cursor.moveToFirst()) {
+            do {
+                val id = cursor.getInt(0)
+                val email = cursor.getString(1)
+                val password = cursor.getString(2)
+                val perfil = cursor.getInt(3)
+                val user = User(id, email, password, perfil)
+                lista.add(user)
+            } while (cursor.moveToNext())
+        }
+        //Cerrar la base de datos
+        db.close()
+        //Retornar el ArrayList
+        return lista
+    }
+}
